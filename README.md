04be1dd02e045cf785655e005bcc6b149cfdd3d7
Helloqiyuan <3302663850@qq.com>
2025-11-19 13:40:02 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,56 @@
+## 张苹果个人博客源代码
+
+页面简洁，功能完善，该博客包含了前后台的代码，只需稍作修改，调用自己的后台接口即可。
+> 本源码是博客完成之后写的，所以几乎和线上博客没差，您可以访问下方地址查看效果。
+
+**源码演示地址**:https://img.zhangpingguo.com/AppleTest/index.html#/home
+
+**线上地址**：https://naive.zhangpingguo.com/
+
+**前端**：Vue3+Vite+Ts+NaiveUI
+
+**注意**：node.js版本：>=18.10.0
+
+## 使用安装
+
+```js
+npm install
+//或
+yarn install
+```
+
+## 运行
+
+```
+npm run dev
+```
+
+## 打包
+
+```
+npm run build
+```
+
+
+> 更多信息可看B站视频(视频后半部分有源码演示)：https://www.bilibili.com/video/BV1bA4m137kH/
+
+
+## 使用到的库：
+
+项目中使用到的插件库，如您在项目中遇到问题，可点击下方文档查看说明文档。
+
+[Naive UI: 一个 Vue 3 组件库](https://www.naiveui.com/zh-CN/light)
+
+[MdEditorV3 Documentation (imzbf.github.io)](https://imzbf.github.io/md-editor-v3/en-US/index)
+
+[Kinesis (aminerman.com)](https://www.aminerman.com/kinesis/?from=madewith.cn#/)
+
+[Presets · @vueuse/motion](https://motion.vueuse.org/features/presets/)
+
+[Undraw UI | 一个 Vue 3 组件库 (gitee.io)](https://undraw.gitee.io/undraw-ui/)
+
+[particles动画](https://particles.js.org/)
+
+[vue-starport/README.zh-Hans.md at main · antfu/vue-starport · GitHub](https://github.com/antfu/vue-starport/blob/main/README.zh-Hans.md)
+
+![输入图片说明](public/static/img/logo.jpg)
---
+++ src/page/Message.vue
@@ -0,0 +1,86 @@
+<template>
+<!--  关于页面-->
+  <BackgroundPlate title="关 于" description="比一个人旅行更寂寞的是，一个人，没钱旅行。" color="#546e7a"></BackgroundPlate>
+  <n-grid :cols="8" id="message">
+
+    <n-gi style="min-height: 100vh" :offset="clientWidth>1075?1:0" :span="clientWidth>1075?6:8">
+      <n-card :embedded="isdarkTheme" :bordered="!isdarkTheme">
+
+        <n-thing>
+          <template #avatar>
+<!--            这个动画效果如何使用。
+                请访问作者文档：https://github.com/antfu/vue-starport/blob/main/README.zh-Hans.md
+-->
+                    <Starport   port="my-id"  style="width: 100px;height: 100px;background: none" >
+                      <myImg ></myImg>
+                    </Starport>
+          </template>
+          <template #header>
+            <n-text tag="div"> Helloqiyuan </n-text>
+          </template>
+
+          <template  #description>
+            目前工作：学生🎗<br>
+            联系邮箱：3302663850@qq.com<br>
+            这个人很简单，没什么好说的。
+          </template>
+        </n-thing>
+
+      </n-card>
+      <br>
+      <n-card :embedded="isdarkTheme" :bordered="!isdarkTheme">
+        <template #header>
+          <n-h2 class="margin-bottom0" prefix="bar" type="success" >
+            关于本站
+          </n-h2>
+        </template>
+        <template #header-extra>
+              <router-link to="/sponsor">
+
+              <!-- <n-button strong secondary><n-icon :size="16"><HeartOutline></HeartOutline></n-icon>赞赏</n-button> -->
+              </router-link>
+
+        </template>
+         <n-h5  class="margin-bottom-top">前端：<n-gradient-text type="error">Vue3+Vite+Ts+NaiveUI， </n-gradient-text>后端：<n-gradient-text type="info">Java</n-gradient-text>，数据库：<n-gradient-text type="info">MySQL</n-gradient-text>；腾讯云服务器。</n-h5>
+         <n-h5  class="margin-bottom-top">本站的编写纯属个人兴趣爱好，前后端均独立完成；主要用来记录自己工作、生活、学习上的笔记。</n-h5>
+         <n-h5  class="margin-bottom-top">本站任何用户注册登录，获取权限后都可发布文章等操作。</n-h5>
+         <n-h5  class="margin-bottom-top">本站前端代码，未考虑IE等低版本浏览器的兼容性。</n-h5>
+         <n-h5  class="margin-bottom-top">本站发布的文章或其他信息如有侵权，请邮件联系我及时删除，谢谢。</n-h5>
+      </n-card>
+      <br>
+      <n-card :embedded="isdarkTheme" :bordered="!isdarkTheme">
+        <template #header>
+          <n-h2 class="margin-bottom0" prefix="bar" type="success" >
+            本站源码
+          </n-h2>
+        </template>
+        <n-card
+            embedded
+            :bordered="false"
+        >
+         本站前端代码已发布到码云：：<n-a href="https://gitee.com/shcmyg/zhang-apple-blog" target="_blank">https://gitee.com/shcmyg/zhang-apple-blog</n-a>。需要的可前往下载。<br>
+          <!-- 如果您觉得本站还不错，可以通过点击右上角的赞赏按钮来支持本站，🧣感谢您的支持！ -->
+        </n-card>
+      </n-card>
+    </n-gi>
+
+  </n-grid>
+</template>
+
+<script setup lang="ts">
+import BackgroundPlate from '../components/background/BackgroundPlate.vue'
+import {HeartOutline} from '@vicons/ionicons5'
+import {VaeStore} from "../store";
+import { Starport } from "vue-starport";
+import {storeToRefs} from "pinia";
+import {inject} from "vue";
+const store = VaeStore();
+let {clientWidth,distanceToBottom,distanceToTop,isdarkTheme} = storeToRefs(store);
+//滚动条回到原位
+const scrollBy = inject<Function>('scrollBy');
+scrollBy? scrollBy(0):''
+</script>
+
+<style scoped>
+
+</style>
